363035736433cd0d7320e6747998bdca5de2de87
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-06-23 02:32:04 +0600

    added auth middleware on route

--- src/app/module/Service/service.route.ts
+++ src/app/module/Service/service.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { ServiceController } from './service.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '../../../../generated/prisma';
 
 const router = express.Router();
 
-router.post('/create', ServiceController.createService);
+router.post(
+  '/create',
+  auth(UserRole.SERVICE_PROVIDER),
+  ServiceController.createService
+);
 
 export const ServiceRoute = router;
--- src/app/module/User/user.route.ts
+++ src/app/module/User/user.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '../../../../generated/prisma';
 
 const router = express.Router();
 
 router.post('/create-user', UserController.createUser);
-router.post('/create-admin', UserController.createAdmin);
+router.post(
+  '/create-admin',
+  auth(UserRole.SUPER_ADMIN),
+  UserController.createAdmin
+);
 router.post('/create-provider', UserController.createServicePorvider);
 
 export const UserRoute = router;
