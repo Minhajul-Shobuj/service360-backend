e699c2e2cc106b7722f53a2259ebca0611197701
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-06-23 16:26:06 +0600

    added zod validation for category and service creation

--- src/app/module/Service/service.route.ts
+++ src/app/module/Service/service.route.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { ServiceController } from './service.controller';
 import auth from '../../middlewares/auth';
 import { UserRole } from '../../../../generated/prisma';
+import validateRequest from '../../middlewares/validateRequest';
+import { ServiceValidation } from './service.validation';
 
 const router = express.Router();
 
 router.post(
   '/create',
   auth(UserRole.SERVICE_PROVIDER),
+  validateRequest(ServiceValidation.createServiceSchemaValidation),
   ServiceController.createService
 );
 
--- src/app/module/User/user.route.ts
+++ src/app/module/User/user.route.ts
@@ -2,15 +2,26 @@ import express from 'express';
 import { UserController } from './user.controller';
 import auth from '../../middlewares/auth';
 import { UserRole } from '../../../../generated/prisma';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.post('/create-user', UserController.createUser);
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserSchemaValidation),
+  UserController.createUser
+);
 router.post(
   '/create-admin',
   auth(UserRole.SUPER_ADMIN),
+  validateRequest(UserValidation.createAdminSchemaValidation),
   UserController.createAdmin
 );
-router.post('/create-provider', UserController.createServicePorvider);
+router.post(
+  '/create-provider',
+  validateRequest(UserValidation.createServiceProviderSchema),
+  UserController.createServicePorvider
+);
 
 export const UserRoute = router;
